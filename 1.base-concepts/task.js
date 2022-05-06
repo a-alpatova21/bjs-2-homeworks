79f81f6abc71e7a392d6c8094c9da04e58d7d060
a-alpatova21 <aas24r@gmail.com>
2022-05-06 19:29:19 +0700

    solved the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
+  return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
