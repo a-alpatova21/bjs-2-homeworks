b8f9b2661d4a7bc8b4ffa29b4fdc12f87956ef19
a-alpatova21 <aas24r@gmail.com>
2022-07-12 22:04:14 +0700

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,31 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
 
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...marks) {
+  this.marks.push(...marks);
+};
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((sum, item) => sum + item) / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+};
